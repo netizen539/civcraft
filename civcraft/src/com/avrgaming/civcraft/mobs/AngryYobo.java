47a5ab0a02b399aa0086d8c08767604706042c79
tyggerr <tygui_quebec@hotmail.com>
2014-03-22 00:09:20 -0400

    Update AngryYobo.java
    Nerfed Yobo drops
    Removed Renforced Braid and Smithy for Elite, added Leather Straps for them
    Removed Artisan Leather for Brutal

--- civcraft/src/com/avrgaming/civcraft/mobs/AngryYobo.java
+++ civcraft/src/com/avrgaming/civcraft/mobs/AngryYobo.java
@@ -93,11 +93,10 @@ public class AngryYobo  extends CommonCustomMob implements ICustomMob {
 		    this.addDrop("mat_carbide_steel_sword_hilt", 0.01);
 		    this.addDrop("mat_carbide_steel_sword_blade", 0.01);
 		    this.addDrop("mat_clay_steel_cast", 0.05);
-		    this.addDrop("mat_reinforced_braid", 0.05);
+		    this.addDrop("mat_leather_straps", 0.05);
 		    this.addDrop("mat_steel_ingot", 0.1);
 
 		    this.addDrop("mat_sticky_resin", 0.1);
-		    this.addDrop("mat_smithy_resin", 0.01);
 		    this.coinDrop(20, 80);
 			break;
 		case BRUTAL:
@@ -110,7 +109,6 @@ public class AngryYobo  extends CommonCustomMob implements ICustomMob {
 		    this.addDrop("mat_tungsten_sword_hilt", 0.01);
 		    this.addDrop("mat_tungsten_sword_blade", 0.01);
 		    this.addDrop("mat_clay_tungsten_casting", 0.05);
-		    this.addDrop("mat_artisan_leather", 0.05);
 		    this.addDrop("mat_steel_ingot", 0.1);
 
 		    this.addDrop("mat_sticky_resin", 0.1);
