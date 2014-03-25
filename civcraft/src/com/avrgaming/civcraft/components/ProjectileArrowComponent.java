4a6d0d0fac6037614dd6611d41df865e5bae3bac
tyggerr <tygui_quebec@hotmail.com>
2014-03-25 19:37:00 -0400

    Update ProjectileArrowComponent.java
    Failed my push... Fixing Oil #2

--- civcraft/src/com/avrgaming/civcraft/components/ProjectileArrowComponent.java
+++ civcraft/src/com/avrgaming/civcraft/components/ProjectileArrowComponent.java
@@ -16,438 +16,78 @@
  * is strictly forbidden unless prior written permission is obtained
  * from AVRGAMING LLC.
  */
-package com.avrgaming.civcraft.command.admin;
+package com.avrgaming.civcraft.components;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import org.bukkit.Location;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.util.Vector;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-
-import com.avrgaming.civcraft.command.CommandBase;
-import com.avrgaming.civcraft.command.ReportChestsTask;
-import com.avrgaming.civcraft.command.ReportPlayerInventoryTask;
+import com.avrgaming.civcraft.cache.ArrowFiredCache;
+import com.avrgaming.civcraft.cache.CivCache;
 import com.avrgaming.civcraft.config.CivSettings;
-import com.avrgaming.civcraft.config.ConfigMaterial;
-import com.avrgaming.civcraft.config.ConfigMaterialCategory;
-import com.avrgaming.civcraft.config.ConfigUnit;
-import com.avrgaming.civcraft.endgame.EndGameCondition;
-import com.avrgaming.civcraft.exception.CivException;
-import com.avrgaming.civcraft.lorestorage.LoreCraftableMaterial;
-import com.avrgaming.civcraft.lorestorage.LoreGuiItem;
-import com.avrgaming.civcraft.lorestorage.LoreGuiItemListener;
-import com.avrgaming.civcraft.lorestorage.LoreMaterial;
-import com.avrgaming.civcraft.main.CivGlobal;
-import com.avrgaming.civcraft.main.CivLog;
-import com.avrgaming.civcraft.main.CivMessage;
-import com.avrgaming.civcraft.main.Colors;
-import com.avrgaming.civcraft.object.Civilization;
-import com.avrgaming.civcraft.object.Resident;
+import com.avrgaming.civcraft.exception.InvalidConfiguration;
+import com.avrgaming.civcraft.object.Buff;
 import com.avrgaming.civcraft.object.Town;
-import com.avrgaming.civcraft.sessiondb.SessionEntry;
-import com.avrgaming.civcraft.threading.TaskMaster;
-import com.avrgaming.civcraft.util.ChunkCoord;
-//import com.avrgaming.civcraft.main.CivGlobal;
-import com.avrgaming.civcraft.util.ItemManager;
+import com.avrgaming.civcraft.structure.Buildable;
+import com.avrgaming.civcraft.util.BlockCoord;
 
-public class AdminCommand extends CommandBase {
+public class ProjectileArrowComponent extends ProjectileComponent {
+	
+	public ProjectileArrowComponent(Buildable buildable, Location turretCenter) {
+		super(buildable, turretCenter);
+	}
 
+	private double power;
+	
 	@Override
-	public void init() {
-		command = "/ad";
-		displayName = "Admin";
-		
-		commands.put("perm", "toggles your permission overrides, if on, ignores all plot permissions.");
-		commands.put("sbperm", "Allows breaking of structure blocks");
-		commands.put("cbinstantbreak", "Allows instant breaking of control blocks.");
-
-		commands.put("maxplayers", "[value] - sets max players to this amount.");
-		commands.put("online", "makes the server online");
-		commands.put("offline", "makes the server offline");
-		commands.put("joinon", "makes the game joinable");
-		commands.put("joinoff", "makes the game not joinable");
-
-		commands.put("recover", "Manage recovery commands");
-		commands.put("server", "shows the name of this server");
-		commands.put("spawnunit", "[unit-id] [town] spawn the unit with this id for this town.");
-
-		commands.put("chestreport", "[radius] check in this radius for chests");
-		commands.put("playerreport", "shows all player ender chest reports.");
-		
-		commands.put("civ", "Admin an individual civilization");
-		commands.put("town", "Admin a town.");
-		commands.put("war", "Manage war settings, turn wars off and on.... etc.");
-		commands.put("lag", "Manage lag on the server by disabling expensive tasks.");	
-		commands.put("camp", "Shows camp management subcommands.");
-		commands.put("chat", "Manage admin chat options, tc, cc, listen etc");
-		commands.put("res", "Manage resident options, settown, setcamp etc");
-		commands.put("build", "Manage buildings. Demolish/repair wonders etc.");
-		commands.put("items", "Opens inventory which allows you to spawn in custom items.");
-		commands.put("item", "Does special things to the item in your hand.");
-		commands.put("timer", "Manage timers.");
-		commands.put("road", "Road management commands");
-		commands.put("clearendgame", "[key] [civ] - clears this end game condition for this civ.");
-		commands.put("endworld", "Starts the Apocalypse.");
-	}
-	
-	public void endworld_cmd() {
-		CivGlobal.endWorld = !CivGlobal.endWorld;
-		if (CivGlobal.endWorld) {			
-			CivMessage.sendSuccess(sender, "It's the end of the world as we know it.");
-		} else {
-			CivMessage.sendSuccess(sender, "I feel fine.");
-		}
-	}
-	
-	public void clearendgame_cmd() throws CivException {
-		String key = getNamedString(1, "enter key.");
-		Civilization civ = getNamedCiv(2);
-		
-		ArrayList<SessionEntry> entries = CivGlobal.getSessionDB().lookup(key);
-		if (entries.size() == 0) {
-			throw new CivException("No end games by that key.");
-		}
-		
-		for (SessionEntry entry : entries) {
-			if (EndGameCondition.getCivFromSessionData(entry.value) == civ) {
-				CivGlobal.getSessionDB().delete(entry.request_id, entry.key);
-				CivMessage.sendSuccess(sender, "Deleted for "+civ.getName());
-			}
-		}		
-	}
-	
-	public void cbinstantbreak_cmd() throws CivException {
-		Resident resident = getResident();
-		
-		resident.setControlBlockInstantBreak(!resident.isControlBlockInstantBreak());
-		CivMessage.sendSuccess(sender, "Set control block instant break:"+resident.isControlBlockInstantBreak());
-	}
-	
-	public static Inventory spawnInventory = null; 
-	public void items_cmd() throws CivException {
-		Player player = getPlayer();
-		
-		if (spawnInventory == null) {
-			spawnInventory = Bukkit.createInventory(player, LoreGuiItem.MAX_INV_SIZE, "Admin Item Spawn");
-			
-			/* Build the Category Inventory. */
-			for (ConfigMaterialCategory cat : ConfigMaterialCategory.getCategories()) {
-				ItemStack infoRec = LoreGuiItem.build(cat.name, 
-						ItemManager.getId(Material.WRITTEN_BOOK), 
-						0, 
-						Colors.LightBlue+cat.materials.size()+" Items",
-						Colors.Gold+"<Click To Open>");
-						infoRec = LoreGuiItem.setAction(infoRec, "openinv:showGuiInv:"+cat.name+" Spawn");
-						spawnInventory.addItem(infoRec);
-						
-				/* Build a new GUI Inventory. */
-				Inventory inv = Bukkit.createInventory(player, LoreGuiItem.MAX_INV_SIZE, cat.name+" Spawn");
-				for (ConfigMaterial mat : cat.materials.values()) {
-					LoreCraftableMaterial craftMat = LoreCraftableMaterial.getCraftMaterialFromId(mat.id);
-					ItemStack stack = LoreMaterial.spawn(craftMat);
-					stack = LoreGuiItem.asGuiItem(stack);
-					stack = LoreGuiItem.setAction(stack, "spawn");
-					inv.addItem(stack);
-					LoreGuiItemListener.guiInventories.put(inv.getName(), inv);			
-				}
-			}
-			
-
-		}
-		
-		player.openInventory(spawnInventory);
-	}
-	
-	public void road_cmd() {
-		AdminRoadCommand cmd = new AdminRoadCommand();	
-		cmd.onCommand(sender, null, "camp", this.stripArgs(args, 1));
-	}
-	
-	public void item_cmd() {
-		AdminItemCommand cmd = new AdminItemCommand();	
-		cmd.onCommand(sender, null, "camp", this.stripArgs(args, 1));
-	}
-	
-	public void timer_cmd() {
-		AdminTimerCommand cmd = new AdminTimerCommand();	
-		cmd.onCommand(sender, null, "camp", this.stripArgs(args, 1));	
-	}
-	
-	public void camp_cmd() {
-		AdminCampCommand cmd = new AdminCampCommand();	
-		cmd.onCommand(sender, null, "camp", this.stripArgs(args, 1));
-	}
-	
-	public void playerreport_cmd() {
-	
-		LinkedList<OfflinePlayer> offplayers = new LinkedList<OfflinePlayer>();
-		for (OfflinePlayer offplayer : Bukkit.getOfflinePlayers()) {
-			offplayers.add(offplayer);
-		}
-		
-		CivMessage.sendHeading(sender, "Players with Goodies");
-		CivMessage.send(sender, "Processing (this may take a while)");
-		TaskMaster.syncTask(new ReportPlayerInventoryTask(sender, offplayers), 0);
-	}
-	
-	public void chestreport_cmd() throws CivException {
-		Integer radius = getNamedInteger(1);
-		Player player = getPlayer();
-		
-		LinkedList<ChunkCoord> coords = new LinkedList<ChunkCoord>();
-		for (int x = -radius; x < radius; x++) {
-			for (int z = -radius; z < radius; z++) {
-				ChunkCoord coord = new ChunkCoord(player.getLocation());
-				coord.setX(coord.getX() + x); coord.setZ(coord.getZ() + z);
-				
-				coords.add(coord);
-			}
-		}
-		
-		CivMessage.sendHeading(sender, "Chests with Goodies");
-		CivMessage.send(sender, "Processing (this may take a while)");
-		TaskMaster.syncTask(new ReportChestsTask(sender, coords), 0);	
-	}
-	
-	public void spawnunit_cmd() throws CivException {		
-		if (args.length < 2) {
-			throw new CivException("Enter a unit id.");
-		}
-		
-		ConfigUnit unit = CivSettings.units.get(args[1]);
-		if (unit == null) {
-			throw new CivException("No unit called "+args[1]);
-		}
-		
-		Player player = getPlayer();
-		Town town = getNamedTown(2);
-		
-//		if (args.length > 2) {
-//			try {
-//				player = CivGlobal.getPlayer(args[2]);
-//			} catch (CivException e) {
-//				throw new CivException("Player "+args[2]+" is not online.");
-//			}
-//		} else {
-//			player = getPlayer();
-//		}
-		
-		Class<?> c;
+	public void loadSettings() {
 		try {
-			c = Class.forName(unit.class_name);
-			Method m = c.getMethod("spawn", Inventory.class, Town.class);
-			m.invoke(null, player.getInventory(), town);
-		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException 
-				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			throw new CivException(e.getMessage());
-		}
-
-		
-		CivMessage.sendSuccess(sender, "Spawned a "+unit.name);
-	}
-	
-	public void server_cmd() {
-		CivMessage.send(sender, Bukkit.getServerName());
-	}
-	
-	
-	public void recover_cmd() {
-		AdminRecoverCommand cmd = new AdminRecoverCommand();	
-		cmd.onCommand(sender, null, "recover", this.stripArgs(args, 1));	
-	}
-	
-	public void town_cmd() {
-		AdminTownCommand cmd = new AdminTownCommand();	
-		cmd.onCommand(sender, null, "town", this.stripArgs(args, 1));
-	}
-	
-	public void civ_cmd() {
-		AdminCivCommand cmd = new AdminCivCommand();	
-		cmd.onCommand(sender, null, "civ", this.stripArgs(args, 1));
-	}
-
-	public void online_cmd() {
-		if (CivGlobal.info != null) {
-			synchronized(CivGlobal.info) {
-				CivGlobal.info.online = true;
-			}
-			CivMessage.sendSuccess(sender, "Server now appears online");
-		}
-	}
-	
-	public void offline_cmd() {
-		if (CivGlobal.info != null) {
-			synchronized(CivGlobal.info) {
-				CivGlobal.info.online = false;
-			}
-			CivMessage.sendSuccess(sender, "Server now appears offline");
-		}
-	}
-	
-	public void joinon_cmd() {
-		if (CivGlobal.info != null) {
-			synchronized(CivGlobal.info) {
-				CivGlobal.info.joinable = true;
-			}
-			CivMessage.sendSuccess(sender, "Players can now join from shotbow");
-		}
-	}
-	
-	public void joinoff_cmd() {
-		if (CivGlobal.info != null) {
-			synchronized(CivGlobal.info) {
-				CivGlobal.info.joinable = false;
-			}
-			CivMessage.sendSuccess(sender, "Players can no longer join from shotbow");
-		}
-	}
-	
-	public void setfullmessage_cmd() {
-		if (args.length < 2) {
-			CivMessage.send(sender, "Current:"+CivGlobal.fullMessage);
-			return;
-		}
-		
-		synchronized(CivGlobal.maxPlayers) {
-			CivGlobal.fullMessage = args[1];
-		}
-		
-		CivMessage.sendSuccess(sender, "Set to:"+args[1]);
-		
-	}
-	
-	public void maxplayers_cmd() throws CivException {
-		if (args.length < 2) {
-			CivMessage.send(sender, "Max players is: "+CivGlobal.maxPlayers);
-			return;
-		}
-		
-		try {
-			Integer size = Integer.valueOf(args[1]);
-			synchronized (CivGlobal.maxPlayers) {
-				CivGlobal.maxPlayers = size;
-			}
+			setDamage(CivSettings.getInteger(CivSettings.warConfig, "arrow_tower.damage"));
+			power = CivSettings.getDouble(CivSettings.warConfig, "arrow_tower.power");
+			range = CivSettings.getDouble(CivSettings.warConfig, "arrow_tower.range");
+			min_range = CivSettings.getDouble(CivSettings.warConfig, "arrow_tower.min_range");
 			
-			if (CivGlobal.info != null) {
-				synchronized (CivGlobal.info) {
-					CivGlobal.info.maxPlayers = size;
-				}
-				
-				CivMessage.sendSuccess(sender, "Set Max players to "+size);
-			}
-		} catch (NumberFormatException e) {
-			throw new CivException ("enter a number");
-		}
-		
-	}
-	
-	public void unban_cmd() throws CivException {
-		if (args.length < 2) {
-			throw new CivException("Enter a player name to ban");
-		}
-		
-		OfflinePlayer offplayer = Bukkit.getOfflinePlayer(args[1]);
-		if (offplayer != null && offplayer.isBanned()) {
-			offplayer.setBanned(false);
-			Resident resident = CivGlobal.getResident(offplayer.getName());
-			if (resident != null) {
-				resident.setBanned(false);
-				resident.setBannedMessage("");
-				resident.save();
-			}
-			CivMessage.sendSuccess(sender, "Unbanned "+args[1]);
-		} else {
-			CivMessage.sendSuccess(sender, "Couldn't find "+args[1]+" or he is not banned.");
+			this.proximityComponent.setBuildable(buildable);
+			this.proximityComponent.setCenter(new BlockCoord(getTurretCenter()));
+			this.proximityComponent.setRadius(range);
+			
+		} catch (InvalidConfiguration e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public void res_cmd() {
-		AdminResCommand cmd = new AdminResCommand();	
-		cmd.onCommand(sender, null, "war", this.stripArgs(args, 1));	}
-	
-	public void chat_cmd() {
-		AdminChatCommand cmd = new AdminChatCommand();	
-		cmd.onCommand(sender, null, "war", this.stripArgs(args, 1));
-	}
-
-	public void war_cmd() {
-		AdminWarCommand cmd = new AdminWarCommand();	
-		cmd.onCommand(sender, null, "war", this.stripArgs(args, 1));
-	}
-	
-	public void lag_cmd() {
-		AdminLagCommand cmd = new AdminLagCommand();	
-		cmd.onCommand(sender, null, "war", this.stripArgs(args, 1));
-	}
-	
-	public void build_cmd() {
-		AdminBuildCommand cmd = new AdminBuildCommand();	
-		cmd.onCommand(sender, null, "war", this.stripArgs(args, 1));
-	}
-	
-	public void perm_cmd() throws CivException {
-		Resident resident = getResident();
-		
-		if (resident.isPermOverride()) {
-			resident.setPermOverride(false);
-			CivMessage.sendSuccess(sender, "Permission override off.");
+	@Override
+	public void fire(Location turretLoc, Entity targetEntity) {
+		if (!buildable.isValid()) {
 			return;
 		}
 		
-		resident.setPermOverride(true);
-		CivMessage.sendSuccess(sender, "Permission override on.");
+		Location playerLoc = targetEntity.getLocation();
+		playerLoc.setY(playerLoc.getY()+1); //Target the head instead of feet.
+					
+		turretLoc = adjustTurretLocation(turretLoc, playerLoc);
+		Vector dir = getVectorBetween(playerLoc, turretLoc).normalize();
+		Arrow arrow = buildable.getCorner().getLocation().getWorld().spawnArrow(turretLoc, dir, (float)power, 0.0f);
+		arrow.setVelocity(dir.multiply(power));
 		
-	}
-	
-	public void sbperm_cmd() throws CivException {
-		Resident resident = getResident();
-		if (resident.isSBPermOverride()) {
-			resident.setSBPermOverride(false);
-			CivMessage.sendSuccess(sender, "Structure Permission override off.");
-			return;
+		if (buildable.getTown().getBuffManager().hasBuff(Buff.FIRE_BOMB)) {
+			arrow.setFireTicks(1000);
 		}
 		
-		resident.setSBPermOverride(true);
-		CivMessage.sendSuccess(sender, "Structure Permission override on.");
+		CivCache.arrowsFired.put(arrow.getUniqueId(), new ArrowFiredCache(this, targetEntity, arrow));
+	}
+
+	public double getPower() {
+		return power;
 	}
 	
+	public void setPower(double power) {
+		this.power = power;
+	}
 	
-
-	
-	@Override
-	public void doDefaultAction() throws CivException {
-		showHelp();
-	}
-
-	@Override
-	public void showHelp() {
-		showBasicHelp();
-	}
-
-	@Override
-	public void permissionCheck() throws CivException {
-		
-		if (sender instanceof Player) {
-			if (((Player)sender).hasPermission(CivSettings.MINI_ADMIN)) {
-				return;
-			}
-		}
-		
-		
-		if (sender.isOp() == false) {
-			throw new CivException("Only admins can use this command.");			
-		}
-	}
-
-	@Override
-	public void doLogging() {
-		CivLog.adminlog(sender.getName(), "/ad "+this.combineArgs(args));
+	public Town getTown() {
+		return buildable.getTown();
 	}
 	
 }
