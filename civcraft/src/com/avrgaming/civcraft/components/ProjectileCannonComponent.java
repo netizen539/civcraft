eeb142adb7bfa34887fb1e73ff291a56b4374a78
tyggerr <tygui_quebec@hotmail.com>
2014-03-25 20:05:16 -0400

    Update ProjectileCannonComponent.java
    Fixing Oil

--- civcraft/src/com/avrgaming/civcraft/components/ProjectileCannonComponent.java
+++ civcraft/src/com/avrgaming/civcraft/components/ProjectileCannonComponent.java
@@ -18,145 +18,240 @@
  */
 package com.avrgaming.civcraft.components;
 
-import java.util.Random;
+import java.util.HashSet;
+
+import net.minecraft.server.v1_7_R1.Vec3D;
 
 import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_7_R1.CraftWorld;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
-import com.avrgaming.civcraft.cache.CannonExplosionProjectile;
-import com.avrgaming.civcraft.config.CivSettings;
-import com.avrgaming.civcraft.exception.InvalidConfiguration;
+import com.avrgaming.civcraft.cache.PlayerLocationCache;
+import com.avrgaming.civcraft.exception.CivException;
+import com.avrgaming.civcraft.main.CivGlobal;
+import com.avrgaming.civcraft.object.Buff;
+import com.avrgaming.civcraft.object.Resident;
 import com.avrgaming.civcraft.structure.Buildable;
 import com.avrgaming.civcraft.threading.TaskMaster;
 import com.avrgaming.civcraft.util.BlockCoord;
 
-public class ProjectileCannonComponent extends ProjectileComponent {
+public abstract class ProjectileComponent extends Component {
+
+	protected int damage;
+	protected double range;
+	protected double min_range;	
+	protected Buildable buildable;
+	protected PlayerProximityComponent proximityComponent;
+	private Location turretCenter;
 	
-	private int damage;
-	private int speed;
-	private int splash;
-	private int fireRate;
-	private int halfSecondCount = 0;
-	
-	public ProjectileCannonComponent(Buildable buildable, Location turretCenter) {
-		super(buildable, turretCenter);
+	private HashSet<BlockCoord> turrets = new HashSet<BlockCoord>();
+
+	public ProjectileComponent(Buildable buildable, Location turretCenter) {
+		this.buildable = buildable;
+		proximityComponent = new PlayerProximityComponent();
+		proximityComponent.createComponent(buildable);
+		this.turretCenter = turretCenter;
+		loadSettings();
+	}
+		
+	@Override
+	public void onLoad() {
 	}
 
 	@Override
-	public void fire(Location turretLoc, Entity targetEntity) {
-		if (halfSecondCount < fireRate) {
-			halfSecondCount++;
-			return;
+	public void onSave() {
+	}
+	
+	/*
+	 * We're overriding the create component class here so that all child-classes
+	 * will register with this method rather than the default. This is done so that the key
+	 * used in components by type will get all instances of this base class rather than having
+	 * to search for the children in the componentByType list.
+	 */
+	@Override
+	public void createComponent(Buildable buildable, boolean async) {
+		if (async) {
+			TaskMaster.asyncTask(new RegisterComponentAsync(buildable, this, ProjectileComponent.class.getName(), true), 0);
 		} else {
-			halfSecondCount = 0;
+			new RegisterComponentAsync(buildable, this, ProjectileComponent.class.getName(), true).run();
 		}
-		
-		class SyncDelay implements Runnable {
-			public double x;
-			public double y;
-			public double z;
-			public World world;
-			
-			@Override
-			public void run() {
-				world.createExplosion(x, y, z, 0.0f, false);
-			}
-		}
-//		
-//		class SyncExplode implements Runnable {
-//			public double x;
-//			public double y;
-//			public double z;
-//			public World world;
-//			
-//			@Override
-//			public void run() {
-//				//world.createExplosion(x, y, z, 1.0f, true);
-//			}
-//		}
-		
-		class SyncFollow implements Runnable {
-			public CannonExplosionProjectile proj;
-			
-			@Override
-			public void run() {
-				
-				if (proj.advance()) {
-					proj = null;
-					return;
-				}
-				TaskMaster.syncTask(this, 1);
-			}
-		}
-		
-		SyncFollow follow = new SyncFollow();
-		CannonExplosionProjectile proj = new CannonExplosionProjectile(buildable, targetEntity.getLocation());
-		proj.setLocation(new Location(turretLoc.getWorld(), turretLoc.getX(), turretLoc.getY(), turretLoc.getZ()));
-		proj.setTargetLocation(targetEntity.getLocation());
-		proj.setSpeed(speed);
-		proj.setDamage(damage);
-		proj.setSplash(splash);
-		follow.proj = proj;
-		TaskMaster.syncTask(follow);
-		
-		World world = turretLoc.getWorld();
-		Random rand = new Random();
-		for (int i = 0; i < 10; i++) {
-			SyncDelay task = new SyncDelay();
-			task.world = world;
-			task.x = turretLoc.getX() + (rand.nextInt(5) - 2.5);
-			task.y = turretLoc.getY() + (rand.nextInt(5) - 2.5);
-			task.z = turretLoc.getZ() + (rand.nextInt(5) - 2.5);
-			TaskMaster.syncTask(task, rand.nextInt(7));
-		}
-	
-//		SyncExplode explode = new SyncExplode();
-//		explode.world = world;
-//		explode.x = targetEntity.getLocation().getX();
-//		explode.y = targetEntity.getLocation().getY();
-//		explode.z = targetEntity.getLocation().getZ();
-		
-//		TaskMaster.syncTask(explode, (int)TimeTools.toTicks(1));
 	}
 	
-	
-
 	@Override
-	public void loadSettings() {
-		try {
-			setDamage(CivSettings.getInteger(CivSettings.warConfig, "cannon_tower.damage"));
-			speed = CivSettings.getInteger(CivSettings.warConfig, "cannon_tower.speed");
-			range = CivSettings.getDouble(CivSettings.warConfig, "cannon_tower.range");
-			min_range = CivSettings.getDouble(CivSettings.warConfig, "cannon_tower.min_range");
-			splash = CivSettings.getInteger(CivSettings.warConfig, "cannon_tower.splash");
-			fireRate = CivSettings.getInteger(CivSettings.warConfig, "cannon_tower.fire_rate");
-			
-			
-			this.proximityComponent.setBuildable(buildable);
-			this.proximityComponent.setCenter(new BlockCoord(getTurretCenter()));
-			this.proximityComponent.setRadius(range);
-		} catch (InvalidConfiguration e) {
-			e.printStackTrace();
-		}
+	public void destroyComponent() {
+		TaskMaster.asyncTask(new RegisterComponentAsync(null, this, ProjectileComponent.class.getName(), false), 0);
+	}	
+	
+	public void setTurretLocation(BlockCoord absCoord) {	
+		turrets.add(absCoord);
 	}
 
+	public Vector getVectorBetween(Location to, Location from) {
+		Vector dir = new Vector();
+		
+		dir.setX(to.getX() - from.getX());
+		dir.setY(to.getY() - from.getY());
+		dir.setZ(to.getZ() - from.getZ());
+	
+		return dir;
+	}
+	
 	public int getDamage() {
-		return damage;
+		double rate = 1;
+		rate += this.getBuildable().getTown().getBuffManager().getEffectiveDouble(Buff.FIRE_BOMB);
+		return (int)(this.damage*rate);
 	}
-
+	
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}
-
-	public int getHalfSecondCount() {
-		return halfSecondCount;
-	}
-
-	public void setHalfSecondCount(int halfSecondCount) {
-		this.halfSecondCount = halfSecondCount;
-	}
-
 	
+	private Location getNearestTurret(Location playerLoc) {
+		
+		double distance = Double.MAX_VALUE;
+		BlockCoord nearest = null;
+		for (BlockCoord turretCoord : turrets) {
+			Location turretLoc = turretCoord.getLocation();
+			if (playerLoc.getWorld() != turretLoc.getWorld()) {
+				return null;
+			}
+			
+			double tmp = turretLoc.distance(playerLoc);
+			if (tmp < distance) {
+				distance = tmp;
+				nearest = turretCoord;
+			}
+			
+		}
+		if (nearest == null) {
+			return null;
+		}
+		return nearest.getLocation();
+	}
+	
+	private  boolean isWithinRange(Location residentLocation, double range) {
+		
+		if (residentLocation.getWorld() != turretCenter.getWorld()) {
+			return false;
+		}
+		
+		if (residentLocation.distance(turretCenter) <= range ) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean canSee(Player player, Location loc2) {
+		Location loc1 = player.getLocation();		
+		return ((CraftWorld)loc1.getWorld()).getHandle().a(Vec3D.a(loc1.getX(), loc1.getY() + player.getEyeHeight(), loc1.getZ()), Vec3D.a(loc2.getX(), loc2.getY(), loc2.getZ())) == null;
+	}
+	
+	protected Location adjustTurretLocation(Location turretLoc, Location playerLoc) {
+		// Keep the y position the same, but advance 1 block in the direction of the player..?
+		int diff = 2;
+		
+		int xdiff = 0;
+		int zdiff = 0;
+		if (playerLoc.getBlockX() > turretLoc.getBlockX()) {
+			xdiff = diff;
+		} else if (playerLoc.getBlockX() < turretLoc.getBlockX()){
+			xdiff = -diff;
+		}  
+		
+		if (playerLoc.getBlockZ() > turretLoc.getBlockZ()) {
+			zdiff = diff;
+		} else if (playerLoc.getBlockZ() < turretLoc.getBlockZ()){
+			zdiff = -diff;
+		} 
+				
+		return turretLoc.getBlock().getRelative(xdiff, 0, zdiff).getLocation();
+	}
+	
+
+	public void process() {
+		if (!buildable.isActive()) {
+			return;
+		}
+		
+		Player nearestPlayer = null;
+		double nearestDistance = Double.MAX_VALUE;
+		
+		Location turretLoc = null;
+		for (PlayerLocationCache pc : proximityComponent.tryGetNearbyPlayers(false)) {
+			if (pc == null || pc.isDead()) {
+				continue;
+			}
+		
+			if (!buildable.getTown().isOutlaw(pc.getName())) {
+				Resident resident = pc.getResident();
+				// Try to exit early by making sure this resident is at war.
+				if (resident == null || (!resident.hasTown())) {
+					continue;
+				}
+				
+				if (!buildable.getCiv().getDiplomacyManager().isHostileWith(resident)) {
+					continue;
+				}
+			}
+			
+			Location playerLoc = pc.getCoord().getLocation();
+			turretLoc = getNearestTurret(playerLoc);
+			if (turretLoc == null) {
+				// No nearest turret, player is probably not in the same world as the turret.
+				return;
+			}
+
+			Player player;
+			try {
+				player = CivGlobal.getPlayer(pc.getName());
+			} catch (CivException e) {
+				return;
+			}
+			
+			// XXX todo convert this to not use a player so we can async...
+			if (!this.canSee(player, turretLoc)) {
+				continue;
+			}
+		
+			if (isWithinRange(player.getLocation(), range)) {
+				if (isWithinRange(player.getLocation(), min_range)) {
+					continue;
+				}
+				
+				double distance = player.getLocation().distance(this.turretCenter);
+				if (distance < nearestDistance) {
+					nearestPlayer = player;
+					nearestDistance = distance;
+				}
+			}
+		}
+		
+		if (nearestPlayer == null || turretLoc == null) {
+			return;
+		}
+		
+		fire(turretLoc, nearestPlayer);	
+	}
+	
+	public abstract void fire(Location turretLoc, Entity targetEntity);
+	public abstract void loadSettings();
+
+	public Buildable getBuildable() {
+		return buildable;
+	}
+	
+	public void setBuildable(Buildable buildable) {
+		this.buildable = buildable;
+	}
+
+	public Location getTurretCenter() {
+		return turretCenter;
+	}
+
+	public void setTurretCenter(Location turretCenter) {
+		this.turretCenter = turretCenter;
+	}
 	
 }
