6b9983f4f0bc8ef0dff833c44e6c60ae4acc7cb3
The-Gunny <benglis84@gmail.com>
2014-04-12 21:02:10 -0500

    Fishing fix. Update to Council of Eight tech.

--- civcraft/src/com/avrgaming/civcraft/fishing/FishingListener.java
+++ civcraft/src/com/avrgaming/civcraft/fishing/FishingListener.java
@@ -12,13 +12,13 @@ import org.bukkit.inventory.ItemStack;
 
 import com.avrgaming.civcraft.config.CivSettings;
 import com.avrgaming.civcraft.config.ConfigFishing;
-import com.avrgaming.civcraft.listener.PlayerListener;
+//import com.avrgaming.civcraft.listener.PlayerListener;
 import com.avrgaming.civcraft.lorestorage.LoreCraftableMaterial;
 import com.avrgaming.civcraft.main.CivMessage;
 import com.avrgaming.civcraft.util.CivColor;
 import com.avrgaming.civcraft.util.ItemManager;
 
-public class FishingListener extends PlayerListener implements Listener {
+public class FishingListener implements Listener {
 	
 	public ArrayList<ConfigFishing> getRandomDrops() {
 		Random rand = new Random();		
